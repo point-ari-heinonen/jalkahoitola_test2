9082212c463901a0fed0a0142144fc432ec4e13b
ari.heinonen@stud.pointcollege.fi <ari.heinonen@stud.pointcollege.fi>
2016-10-14 08:11:06 +0300

    Eka commit

---
+++ Jalkahoitola_test2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Jalkahoitola_test2.Startup))]
+namespace Jalkahoitola_test2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
